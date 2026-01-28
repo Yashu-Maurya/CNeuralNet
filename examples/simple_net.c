1bbf09233dcb8c2dfd7d25b8f8d5e2c562a7cf01
Yashu Maurya <yashuff1@gmail.com>
2026-01-28 09:28:04 +0530

    Changed to Linear Activation

--- examples/simple_net.c
+++ examples/simple_net.c
@@ -6,6 +6,9 @@
 #define LEARNING_RATE 0.1
 #define EPOCHS 1000
 
+Matrix* run_model(Matrix* inputs, Matrix* weights, Matrix* bias);
+void train_model(Matrix* out, float target, Matrix *inputs, Matrix *weights, Matrix *bias);
+
 int main() {
     // f(x) = 2x
     Matrix* inputs = create_matrix(1, 1);   // a 1x1 input matrix.
@@ -23,40 +26,46 @@ int main() {
             float target = i * 2.0f / 20.f;
 
             Matrix* out;
-            out = multiply_mat(weights, inputs);
-            add_matrix(out, bias);
-            matrix_sigmoid(out);
-
-            float error = target - out->data[0];
-            print_matrix(out);
-            printf("Error %i: %f\t", i, error);
-
-            float derivative = out->data[0] * (1 - out->data[0]);
-            printf("Derivative %i: %f\t", i, derivative);
-
-            float gradient = error * derivative * inputs->data[0];
-            printf("Gradient %i: %f\t", i, gradient);
-
-            float bias_gradient = error * derivative * 1;
-            printf("Bias Gradient %i: %f\t", i, bias_gradient);
-
-            add_scaler(weights, (LEARNING_RATE * gradient));
-            add_scaler(bias, (LEARNING_RATE * bias_gradient));
+            out = run_model(inputs, weights, bias);
+            train_model(out, target, inputs, weights, bias);
 
             free_matrix(out);
         }
-        
+
         printf("\nEPOCH %i DONE \n", j);
     }
-        inputs->data[0] = 5 / 20.0f;
-        Matrix* out;
-        out = multiply_mat(weights, inputs);
-        add_matrix(out, bias);
-        matrix_sigmoid(out);
-        printf("\n\nFinal Inference Test for 5: ");
-        out->data[0] = out->data[0] * 20.0f;
-        print_matrix(out);
-        free_matrix(out);
+    inputs->data[0] = 15 / 20.0f;
+    Matrix* out;
+    out = run_model(inputs, weights, bias);
+    printf("\n\nFinal Inference Test for 5: ");
+    out->data[0] = out->data[0] * 20.0f;
+    print_matrix(out);
+    
+    free_matrix(out);
+    free_matrix(inputs);
+    free_matrix(weights);
+    free_matrix(bias);
 
     return 0;
+}
+
+Matrix* run_model(Matrix* inputs, Matrix* weights, Matrix* bias) {
+    Matrix* out;
+    out = multiply_mat(weights, inputs);
+    add_matrix(out, bias);
+    return out;
+}
+
+void train_model(Matrix* out, float target, Matrix *inputs, Matrix *weights, Matrix *bias) {
+    float error = target - out->data[0];
+    print_matrix(out);
+
+    float derivative = 1.0f; // linear activation
+
+    float gradient = error * derivative * inputs->data[0];
+
+    float bias_gradient = error * derivative * 1;
+
+    add_scaler(weights, (LEARNING_RATE * gradient));
+    add_scaler(bias, (LEARNING_RATE * bias_gradient));
 }
