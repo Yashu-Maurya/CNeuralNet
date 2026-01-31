fac2360074088b3bcc9493a5016f44a33ec63781
Yashu Maurya <yashuff1@gmail.com>
2026-02-01 00:09:49 +0530

    UNCHECKED: network struct and functions

---
+++ include/network.h
@@ -0,0 +1,19 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include "layer.h"
+
+typedef struct Network Network;
+
+struct Network {
+    Layer **layers;
+    int layer_count;
+};
+
+Network* create_network();
+void add_layer(Network *n, Layer *l);
+void free_network(Network *n);
+void train_network(Network *n, Matrix *inputs, Matrix* targets, float learning_rate);
+Matrix* predict_network(Network *n, float *input);
+
+#endif
---
+++ src/network.c
@@ -0,0 +1,96 @@
+#include "../include/network.h"
+
+Network* create_network() {
+    Network* n = (Network*)malloc(sizeof(Network));
+    if (n == NULL) {
+        perror("Error Assigning Memory for Network.\n");
+        return NULL;
+    }
+    n->layers = NULL;
+    n->layer_count = 0;
+    return n;
+}
+
+void free_network(Network* n) {
+    if (n == NULL) {
+        perror("Can't Find Network to free from memory\n");
+        return;
+    }
+
+    for (int i = 0; i < n->layer_count; i += 1) {
+        if (n->layers[i] != NULL) {
+            free_layer(n->layers[i]);
+        }
+    }
+
+    if (n->layers != NULL) {
+        free(n->layers);
+    }
+
+    free(n);
+    return;
+}
+
+void add_layer(Network* n, Layer* l) {
+    if (l == NULL) {
+        perror("Error Adding Layer, Layer is NULL\n");
+        return;
+    }
+
+    if (n == NULL) {
+        perror("Error Adding Layer, Network is NULL\n");
+        return;
+    }
+
+    int nc = n->layer_count + 1;
+    Layer** temp = realloc(n->layers, nc * sizeof(Layer*));
+    if (temp == NULL) {
+        perror("Error Reallocating memory to the layer pointer.\n");
+        return;
+    }
+
+    n->layers = temp;
+    n->layers[n->layer_count] = l;
+    n->layer_count = nc;
+
+    return;
+}
+
+Matrix* predict_network(Network* n, float* input) {
+    if (n == NULL) {
+        perror("Network is NULL, Can't predict. \n");
+        return NULL;
+    }
+    if (input == NULL) {
+        perror("Input is Empty \n");
+        return;
+    }
+    Matrix* out = input;
+
+    for (int i = 0; i < n->layer_count; i++) {
+        out = layer_forward(n->layers[i], out);
+    }
+
+    return out;
+}
+
+void train_network(Network* n, Matrix* input, Matrix* target, float learning_rate) {
+    if (n == NULL || input == NULL || target == NULL) return;
+
+    Matrix* prediction = network_forward(n, input);
+    Matrix* loss_gradient = subtract_matrix(prediction, target);
+    Matrix* current_gradient = loss_gradient;
+
+    for (int i = n->layer_count - 1; i >= 0; i--) {
+        Matrix* next_gradient = layer_backward(n->layers[i], current_gradient, learning_rate);
+        
+        if (i < n->layer_count - 1) {
+             free_matrix(current_gradient);
+        }
+        
+        current_gradient = next_gradient;
+    }
+
+    free_matrix(current_gradient);
+    free_matrix(loss_gradient);
+}
